daa7ca9716c93b8d237e6c0eca0a1e5bd9dc2ba3
Kevin <songtao046@126.com>
2022-08-03 17:53:26 +0800

    add sha2

--- Sources/CryptoSwift/SHA2.swift
+++ Sources/CryptoSwift/SHA2.swift
@@ -45,7 +45,7 @@ public final class SHA2: DigestType {
 
   @frozen
   public enum Variant: RawRepresentable {
-    case sha224, sha256, sha384, sha512
+    case sha224, sha256, sha384, sha512, sha512_256
 
     public var digestLength: Int {
       self.rawValue / 8
@@ -55,7 +55,7 @@ public final class SHA2: DigestType {
       switch self {
         case .sha224, .sha256:
           return 64
-        case .sha384, .sha512:
+        case .sha384, .sha512, .sha512_256:
           return 128
       }
     }
@@ -71,6 +71,8 @@ public final class SHA2: DigestType {
           return 384
         case .sha512:
           return 512
+        case .sha512_256:
+          return 5122
       }
     }
 
@@ -83,6 +85,8 @@ public final class SHA2: DigestType {
         case 384:
           self = .sha384
         case 512:
+          self = .sha512
+        case 5122:
           self = .sha512
         default:
           return nil
@@ -98,7 +102,7 @@ public final class SHA2: DigestType {
           return [0x6a09e667, 0xbb67ae85, 0x3c6ef372, 0xa54ff53a, 0x510e527f, 0x9b05688c, 0x1f83d9ab, 0x5be0cd19]
         case .sha384:
           return [0xcbbb9d5dc1059ed8, 0x629a292a367cd507, 0x9159015a3070dd17, 0x152fecd8f70e5939, 0x67332667ffc00b31, 0x8eb44a8768581511, 0xdb0c2e0d64f98fa7, 0x47b5481dbefa4fa4]
-        case .sha512:
+        case .sha512, .sha512_256:
           return [0x6a09e667f3bcc908, 0xbb67ae8584caa73b, 0x3c6ef372fe94f82b, 0xa54ff53a5f1d36f1, 0x510e527fade682d1, 0x9b05688c2b3e6c1f, 0x1f83d9abfb41bd6b, 0x5be0cd19137e2179]
       }
     }
@@ -119,7 +123,7 @@ public final class SHA2: DigestType {
   public init(variant: SHA2.Variant) {
     self.variant = variant
     switch self.variant {
-      case .sha224, .sha256:
+    case .sha224, .sha256, .sha512_256:
         self.accumulatedHash32 = variant.h.map { UInt32($0) } // FIXME: UInt64 for process64
         self.blockSize = variant.blockSize
         self.size = variant.rawValue
@@ -315,7 +319,7 @@ extension SHA2: Updatable {
         switch self.variant {
           case .sha224, .sha256:
             self.process32(block: chunk, currentHash: &self.accumulatedHash32)
-          case .sha384, .sha512:
+          case .sha384, .sha512, .sha512_256:
             self.process64(block: chunk, currentHash: &self.accumulatedHash64)
           }
         processedBytes += chunk.count
@@ -337,7 +341,7 @@ extension SHA2: Updatable {
           result[pos + 3] = UInt8(h & 0xff)
           pos += 4
         }
-      case .sha384, .sha512:
+      case .sha384, .sha512, .sha512_256:
         var pos = 0
         for idx in 0..<self.accumulatedHash64.count where idx < self.variant.finalLength {
           let h = accumulatedHash64[idx]
@@ -358,7 +362,7 @@ extension SHA2: Updatable {
       switch self.variant {
         case .sha224, .sha256:
           self.accumulatedHash32 = self.variant.h.lazy.map { UInt32($0) } // FIXME: UInt64 for process64
-        case .sha384, .sha512:
+        case .sha384, .sha512, .sha512_256:
           self.accumulatedHash64 = self.variant.h
       }
     }
